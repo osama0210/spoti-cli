e46866a73ee7fb89958d4d1c83cce443a456dee4
tamz-z <1206072@student.roc-nijmegen.nl>
2025-06-02 12:12:09 +0200

    added Song fuction variables

--- Song.cs
+++ Song.cs
@@ -1,6 +1,24 @@
 namespace spotiCLI;
 
-public class Song
+public class SongS
 {
-    
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public string Album { get; set; }
+    public int Year { get; set; }
+    public Genres Genre { get; set; }
+
+    public SongS(string title, string artist, string album, int year, Genres genre)
+    {
+        Title = title;
+        Artist = artist;
+        Album = album;
+        Year = year;
+        Genre = genre;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} by {Artist} from the album {Album} ({Year}) - Genre: {Genre}";
+    }
 }
