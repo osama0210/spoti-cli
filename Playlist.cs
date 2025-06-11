178ff468721205b36c0a9b896620fba523766492
osama0210 <osama.asmi04@gmail.com>
2025-06-11 17:17:01 +0200

    feat: Implemented basic playlist creation via Client and Library (US-001)

--- Playlist.cs
+++ Playlist.cs
@@ -6,6 +6,13 @@ public class Playlist : SongCollection
     private bool isPublic = true;
     
     public bool IsPublic { get; set; }
+
+    public Playlist(string title, Person owner, bool isPublic)
+    {
+        this.Title = title;
+        this.owner = owner;
+        this.IsPublic = isPublic;
+    }
     
     // Met deze methode voeg ik een Song-object toe aan de collectie.
     // Dit gebruik ik later om nieuwe liedjes aan het programma toe te voegen.
@@ -17,5 +24,10 @@ public class Playlist : SongCollection
     public void RemoveSong(Song song)
     {
         this.songs.Remove(song);
+    }
+    
+    public override string ToString()
+    {
+        return $"Playlist: {Title}, Owner: {owner.Name}, Public: {IsPublic}";
     }
 }
