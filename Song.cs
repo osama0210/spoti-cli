324632054cf74c99e63d8915cc2ebae2851d87d9
tamz-z <1206072@student.roc-nijmegen.nl>
2025-06-19 12:48:41 +0200

    Added songs:new song

--- Song.cs
+++ Song.cs
@@ -21,6 +21,23 @@ public class Song
         Favorite = false;
     }
 
+    public static List<Song> Songs10()
+    {
+        return new List<Song>
+      {
+        new Song("Starlit Avenue", "Nova Lane", "Midnight Drive", 2022, Genres.Pop),
+        new Song("Echoes of Tomorrow", "The Wanderers", "Future Sounds", 2021, Genres.Electronic),
+        new Song("Crimson Skies", "Scarlet Road", "Red Horizon", 2020, Genres.Rock),
+        new Song("Silent Whispers", "Luna Grey", "Moonlit Tales", 2023, Genres.Jazz),
+        new Song("Golden Leaves", "Autumn Breeze", "Seasons", 2019, Genres.Classical),
+        new Song("Neon Dreams", "Pulse City", "Electric Nights", 2024, Genres.HipHop),
+        new Song("Shadow Dance", "Velvet Mist", "Hidden Steps", 2022, Genres.RnB),
+        new Song("Frozen Time", "Arctic Flow", "Winter Chill", 2020, Genres.Metal),
+        new Song("Paper Boats", "River Song", "Drift Away", 2021, Genres.Country),
+        new Song("Sunrise Parade", "Morning Glow", "New Dawn", 2023, Genres.Pop)
+      };
+    }
+
     public void DisplayLyrics()
     {
         Console.WriteLine($"tittle van {Title}:\n{Lyrics}");
@@ -31,3 +48,5 @@ public class Song
         return $"{Title} gemaakt door {Artist} van de album '{Album}' ({Year}) - Genre: {Genre}";
     }
 }
+
+    
